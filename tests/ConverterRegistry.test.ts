e4718d36436baaf27b652fc13c18ec250e3319ee
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-01-18 14:53:09 +0200

    One passing unit test

--- tests/ConverterRegistry.test.ts
+++ tests/ConverterRegistry.test.ts
@@ -1,16 +1,20 @@
 import { clearStore, test, assert } from 'matchstick-as/assembly/index'
 import { LiquidityPoolAdded as LiquidityPoolAddedSchema } from '../generated/schema'
-import { log } from '@graphprotocol/graph-ts'
+import { Address, Bytes, ethereum, log } from '@graphprotocol/graph-ts'
 // import { LiquidityPoolAdded } from '../../generated/ConverterRegistry/ConverterRegistry'
 // import { handleLiquidityPoolAdded } from '../ConverterRegistry'
 
 log.info('RUNNING THIS FILE (CONVERTER)', [])
 
-test('Testing the test function', () => {
-  log.info('Running this test...', [])
-  let liquidityPool = new LiquidityPoolAddedSchema('0x70d228bc14fa78304ca05db387dfd7f50a90cbe2')
+test('Saving a new liquidity pool', () => {
+  const txHash = '0x0087b4e9d9ca7ad9fb9a098b959c0451a9852db1873261f5fa8537f704d7f83d'
+  const logIndex = 5
+  const addressString = '0x70d228bc14fa78304ca05db387dfd7f50a90cbe2'
+  const poolAddress = Address.fromString(addressString)
+  let liquidityPool = new LiquidityPoolAddedSchema(txHash + logIndex.toString())
+  liquidityPool._liquidityPool = poolAddress
   liquidityPool.save()
-  assert.fieldEquals('LiquidityPoolAdded', '0x70d228bc14fa78304ca05db387dfd7f50a90cbe2', '_liquidityPool', '0x70d228bc14fa78304ca05db387dfd7f50a90cbe2')
+  assert.fieldEquals('LiquidityPoolAdded', txHash + logIndex.toString(), '_liquidityPool', addressString)
 
   clearStore()
 })
